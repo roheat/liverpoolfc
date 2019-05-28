6486f47ceaae36247966df014a517535946c2534
Rohith Srivathsav <rohithroxtar@gmail.com>
2019-05-28 11:34:23 +0530

    firebase.js added, export file updated

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEeZQj_NCBHDl1tsE-eTOszZYDAvZvW08",
+  authDomain: "liverpoolfc-2df96.firebaseapp.com",
+  databaseURL: "https://liverpoolfc-2df96.firebaseio.com",
+  projectId: "liverpoolfc-2df96",
+  storageBucket: "liverpoolfc-2df96.appspot.com",
+  messagingSenderId: "614576990786",
+  appId: "1:614576990786:web:658baa21db188579"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
